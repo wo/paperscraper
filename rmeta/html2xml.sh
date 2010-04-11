b9abf79826bfa98de6ecb4db9a055d660fd3c5fb
Wolfgang Schwarz <wo@umsu.de>
2010-04-11 10:53:20 -0500

    tidying up rmeta directory

--- rmeta/html2xml.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-/usr/bin/xvfb-run -n 99 /opt/xulrunner/xulrunner /home/wo/html2xml/application.ini $1
--- rmeta/html2xml/defaults/preferences/html2xml-prefs.js
+++
@@ -1,7 +0,0 @@
-pref("toolkit.defaultChromeURI", "chrome://html2xml/content/html2xml.xul");
-pref("general.useragent.extra.mybrowser", "html2xml/0.1");
-pref("browser.dom.window.dump.enabled", true);
-pref("javascript.options.showInConsole", true);
-pref("javascript.options.strict", true);
-pref("nglayout.debug.disable_xul_cache", true);
-pref("nglayout.debug.disable_xul_fastload", true);
