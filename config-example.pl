5380bc8a55aaeea5f504ab27a22f3413ff094e29
Wolfgang Schwarz <wo@umsu.de>
2010-10-11 16:24:29 +1100

    move config settings from rss and update_sources to config.pl

--- config-example.pl
+++ config-example.pl
@@ -1,19 +1,29 @@
 
 # Database connection:
-MYSQL_DB   => 'database',
-MYSQL_USER => 'user',
-MYSQL_PASS => 'pass',
+MYSQL_DB    => 'database',
+MYSQL_USER  => 'user',
+MYSQL_PASS  => 'pass',
 
 # Installation root (typically where this file lives):
-PATH       => '/home/wo/projects/opp-tools/',
+PATH        => '/home/wo/projects/opp-tools/',
 
 # Process papers with these filetypes:
-FILETYPES  => ['pdf', 'ps', 'doc', 'rtf', 'html', 'txt'],
+FILETYPES   => ['pdf', 'ps', 'doc', 'rtf', 'html', 'txt'],
 
 # Number of links to process in one go:
-NUM_URLS   => 5,
+NUM_URLS    => 5,
 
 # Software:
-PS2PDF      => '/usr/bin/ps2pdf',
-UNOCONV     => '/usr/bin/unoconv',
-WKHTMLTOPDF => '/usr/bin/wkhtmltopdf',
+PS2PDF       => '/usr/bin/ps2pdf',
+UNOCONV      => '/usr/bin/unoconv',
+WKHTMLTOPDF  => '/usr/bin/wkhtmltopdf',
+
+# Maximum spamminess score for entries in the RSS feed:
+SPAM_THRESHOLD        => 0.4,
+
+# Minimum parser confidence for entries in the RSS feed:
+CONFIDENCE_THRESHOLD  => 0.6,
+
+# CGI access to update_sources is restricted to IP addresses matching
+# the following pattern:
+ALLOWED_IPS  => '127.0.0.1|150.203.224.249',
