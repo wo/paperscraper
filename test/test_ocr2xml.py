09500b7764e35726088a5d98637640b1fd073faa
wo <wo@umsu.de>
2016-02-20 14:53:14 +0000

    convert unittest to pytest

--- test/test_ocr2xml.py
+++ test/test_ocr2xml.py
@@ -1,28 +1,23 @@
 #!/usr/bin/env python3
-import unittest
+import pytest
 import re
 import lxml.etree
 import os.path
-from docparser.pdf2xml import ocr2xml
+from docparser import ocr2xml
 
 curpath = os.path.abspath(os.path.dirname(__file__))
 testdir = os.path.join(curpath, 'testdocs')
 
-class PDFTest(object):
+class PDFTest():
     """common setup and test methods for all pdfs"""
 
-    xml = None
- 
-    def setUp(self):
+    def __init__(self, pdffile):
         """call ocr2xml and read output into etree document self.xml"""
-        if self.xml is not None:
-            return True
-        print('\n'+self.pdffile)
-        xmlfile = self.pdffile.replace('.pdf', '.ocr.xml')
-        ocr2xml.ocr2xml(self.pdffile, xmlfile, debug_level=0)
+        xmlfile = pdffile.replace('.pdf', '.ocr.xml')
+        ocr2xml.ocr2xml(pdffile, xmlfile, debug_level=0)
         with open(xmlfile, 'rb') as f:
             xml = f.read()
-        self.__class__.xml = lxml.etree.fromstring(xml)
+        self.xml = lxml.etree.fromstring(xml)
     
     def get_page(self, pagenum):
         """etree representation of page"""
@@ -46,47 +41,53 @@ class PDFTest(object):
         page = self.get_page(page)
         fontsizes = page.xpath('./fontspec/@size')
         return sorted([int(fs) for fs in fontsizes])
-    
-class A_SimplePDFTest(PDFTest, unittest.TestCase):
 
+
+@pytest.fixture(scope="module")
+def simplepdf():
     pdffile = os.path.join(testdir, 'simple.pdf')
+    return PDFTest(pdffile)
 
-    def test_fontspecs(self):
-        """testing number of fontspec elements"""
-        self.assertEqual(self.num_fontspecs(), 4)
-    
-    def test_fontsizes(self):
-        """testing font sizes"""
-        fontsizes = [12,15,19,23]
-        for i,fs in enumerate(self.fontsizes()):
-            with self.subTest(i=i):
-                self.assertAlmostEqual(fs, fontsizes[i], delta=1)
+def test_simple_num_fontspecs(simplepdf, caplog):
+    """testing number of fontspec elements"""
+    assert simplepdf.num_fontspecs() == 4
 
-    def test_titleheight(self):
-        """testing height of title text"""
-        title = self.get_element('ipsum', page=1)
-        height = int(title.xpath('@height')[0])
-        self.assertAlmostEqual(height, 23, delta=3)
+def test_simple_fontsizes(simplepdf, caplog):
+    """testing font sizes"""
+    fontsizes = [12,15,19,23]
+    for i,fs in enumerate(simplepdf.fontsizes()):
+        assert fontsizes[i]-1 <= fs <= fontsizes[i]+1
 
-class B_CarnapPDFTest(PDFTest, unittest.TestCase):
+def test_simple_titleheight(simplepdf, caplog):
+    """testing height of title text"""
+    title = simplepdf.get_element('ipsum', page=1)
+    height = int(title.xpath('@height')[0])
+    assert 20 <= height <= 26
 
+
+@pytest.fixture(scope="module")
+def carnappdf():
     pdffile = os.path.join(testdir, 'carnap-short.pdf')
+    return PDFTest(pdffile)
 
-    def test_num_fontspecs(self):
-        self.assertIn(self.num_fontspecs(), [4,5])
-    
-    def test_fontsizes(self):
-        fontsizes = [9,10,12,14,15]
-        for i,fs in enumerate(self.fontsizes()):
-            with self.subTest(i=i):
-                self.assertAlmostEqual(fs, fontsizes[i], delta=1)
+def test_carnap_num_fontspecs(carnappdf, caplog):
+    """testing number of fontspec elements"""
+    assert 4 <= carnappdf.num_fontspecs() <= 5
 
-    def test_uniform_content_font(self):
-        page = self.get_page(1)
-        fonts = page.xpath('./text/@font')
-        for i in range(6,45):
-            with self.subTest(i=i):
-                self.assertEqual(fonts[i], fonts[i-1])
+def test_carnap_fontsizes(carnappdf, caplog):
+    """testing font sizes"""
+    fontsizes = [9,10,12,14,15]
+    for i,fs in enumerate(carnappdf.fontsizes()):
+        assert fontsizes[i]-1 <= fs <= fontsizes[i]+1
+        
+def test_carnap_uniformfont(carnappdf, caplog):
+    """testing uniform content font"""
+    page = carnappdf.get_page(1)
+    fonts = page.xpath('./text/@font')
+    for i in range(6,45):
+        assert fonts[i] == fonts[i-1]
+
+#pdffile = os.path.join(testdir, 'fleurbaeymonginSCW05.pdf')
 
 if __name__ == '__main__':
     unittest.main()
