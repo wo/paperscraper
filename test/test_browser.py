7b79586587ac594a216d37357532024f882db71f
Wolfgang Schwarz <wo@umsu.de>
2016-04-14 11:38:44 +0100

    fix test

--- test/test_browser.py
+++ test/test_browser.py
@@ -8,11 +8,12 @@ from browser import Browser
 
 VDISPLAY = True
 
+curpath = os.path.abspath(os.path.dirname(__file__))
+testdir = os.path.join(curpath, 'sourcepages')
+
 def test_status(caplog):
     caplog.setLevel(logging.CRITICAL, logger='selenium')
     caplog.setLevel(logging.DEBUG, logger='opp')
-    curpath = os.path.abspath(os.path.dirname(__file__))
-    testdir = os.path.join(curpath, 'sourcepages')
     b = Browser(use_virtual_display=VDISPLAY)
     src = 'file://'+testdir+'/umsu.html'
     b.goto(src)
@@ -20,24 +21,23 @@ def test_status(caplog):
     src = 'file://'+testdir+'/xxx.html'
     b.goto(src)
     assert b.status == 404
+    del b
 
 def test_reuse(caplog):
     caplog.setLevel(logging.CRITICAL, logger='selenium')
     caplog.setLevel(logging.DEBUG, logger='opp')
-    curpath = os.path.abspath(os.path.dirname(__file__))
-    testdir = os.path.join(curpath, 'sourcepages')
     b1 = Browser(reuse_browser=True, use_virtual_display=VDISPLAY)
     src = 'file://'+testdir+'/umsu.html'
     b1.goto(src)
     b2 = Browser(reuse_browser=True, use_virtual_display=VDISPLAY)
     b2.goto(src)
     assert b1 == b2
+    del b1
+    del b2
 
 def test_xvfb(caplog):
     caplog.setLevel(logging.CRITICAL, logger='selenium')
     caplog.setLevel(logging.DEBUG, logger='opp')
-    curpath = os.path.abspath(os.path.dirname(__file__))
-    testdir = os.path.join(curpath, 'sourcepages')
     b = Browser(use_virtual_display=True)
     src = 'file://'+testdir+'/umsu.html'
     b.goto(src)
