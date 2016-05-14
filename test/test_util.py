3a375be4db829fa43e3d07a938c43dbc13f9d33f
wo <wo@umsu.de>
2016-05-14 13:02:33 +0100

    adding test case for previous commit

---
+++ test/test_util.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import pytest
+import os
+import util
+
+def test_text_content():
+    curpath = os.path.abspath(os.path.dirname(__file__))
+    testdoc = os.path.join(curpath, 'testdocs', 'carnap-short.xml')
+    content = util.text_content(testdoc)
+    assert '<' not in content
+    assert 'sys-' not in content
+    assert 'systems' in content
+
