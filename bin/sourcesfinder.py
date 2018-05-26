4586b4814ab223bdf37fd6f0c22cfc665ed18729
wo <wo@umsu.de>
2018-05-26 11:14:47 +0100

    improve sourcesfinder

--- bin/sourcesfinder.py
+++ bin/sourcesfinder.py
@@ -114,7 +114,7 @@ class SourcesFinder:
     BAD_URL_PARTS = [
         'jstor.org', 'springer.com', 'wiley.com', 'journals.org',
         'tandfonline.com', 'ssrn.com', 'oup.com', 'mitpress.mit.edu',
-        'plato.stanford.edu',
+        'plato.stanford.edu', 'scribd.com', 'archive.org',
         'umich.edu/e/ergo', 'cambridge.org', 'hugendubel.',
         'dblp.uni-trier', 'citec.repec.org', 'publicationslist.org',
         'philarchive.org', 'aristoteliansociety.org.uk/the-proceedings',
