7fec4254371888f12d59cfc09c0709a12fac6539
wo <wo@umsu.de>
2018-09-13 10:02:45 +0100

    add cron script to kill and restart everything

---
+++ bin/restart.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+
+/usr/bin/python3 /home/wo/opp-tools/bin/scraperdemon.py stop
+sleep 2m
+pkill -9 scraperdemon
+killall firefox-bin
+killall geckodriver
+killall -9 firefox-bin
+killall -9 geckodriver
+
