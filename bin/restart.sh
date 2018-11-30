ff87f116a3e027b478791edcb8ce817f0b140864
wo <wo@umsu.de>
2018-11-30 10:19:44 +0000

    tidy up /tmp on restart

--- bin/restart.sh
+++ bin/restart.sh
@@ -8,4 +8,6 @@ killall geckodriver
 killall -9 firefox-bin
 killall -9 geckodriver
 rm /tmp/opp-scraper.pid
+rm -rvf /tmp/rust_mozprofile.*
+rm -rvf /tmp/tmp*
 /usr/bin/python3 /home/wo/opp-tools/bin/scraperdaemon.py start
